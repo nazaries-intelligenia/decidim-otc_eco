cb421dcd2ec93a0a216f29f19515393a0e9e5d49
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-10-07 13:10:42 +0300

    Lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-Rails.application.routes.draw do
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+# frozen_string_literal: true
 
-  mount Decidim::Core::Engine => '/'
+Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  mount Decidim::Core::Engine => "/"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
