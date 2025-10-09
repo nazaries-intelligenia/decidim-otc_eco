18b062f3390342d78343f4f7520faaaaf2ae4071
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-10-09 18:15:40 +0300

    Disable user group creation for non admin users

---
+++ spec/lib/overrides_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# We make sure that the checksum of the file overridden is the same
+# as the expected. If this test fails, it means that the overridden
+# file should be updated to match any change/bug fix introduced in the core
+checksums = [
+  {
+    package: "decidim-core",
+    files: {
+      "/app/permissions/decidim/permissions.rb" => "89cdeb04aca3775b876bf497ac3cc2fb",
+      "/app/cells/decidim/profile_actions_cell.rb" => "58dc7248028205c01a370243fb7b6b6f"
+    }
+  }
+]
+
+describe "Overridden files", type: :view do
+  checksums.each do |item|
+    spec = Gem::Specification.find_by_name(item[:package])
+
+    item[:files].each do |file, signature|
+      next unless spec
+
+      it "#{spec.gem_dir}#{file} matches checksum" do
+        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+      end
+    end
+  end
+
+  private
+
+  def md5(file)
+    Digest::MD5.hexdigest(File.read(file))
+  end
+end
