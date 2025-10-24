a498b93e8c642626e2cfa2f6ee735c636637f811
Alejandro Puerta <71900287+apuertadelaguila@users.noreply.github.com>
2025-10-24 12:06:17 +0200

    Creates initial debates (#8)
    * Add initial debates with translations for debates component
    
    * en and es locales normalized

--- app/commands/concerns/decidim/create_user_group_override.rb
+++ app/commands/concerns/decidim/create_user_group_override.rb
@@ -16,6 +16,7 @@ module Decidim
           create_assembly
           create_main_data_content_block
           create_debates_component
+          create_initial_debates
           add_members_as_private_users
         end
         notify_admins
@@ -69,7 +70,7 @@ module Decidim
       end
 
       def create_debates_component
-        Decidim::Component.create!(
+        @debates_component = Decidim::Component.create!(
           manifest_name: :debates,
           name: { form.current_organization.default_locale => "Foro" },
           participatory_space: @assembly,
@@ -78,6 +79,30 @@ module Decidim
             comments_enabled: true
           }
         )
+      end
+
+      def create_initial_debates
+        debate_keys = [
+          :steering_committee,
+          :community_notices,
+          :driving_group,
+          :projects_financing,
+          :community_dynamization,
+          :alliances_sustainability,
+          :improvement_suggestions,
+          :energy_saving
+        ]
+
+        debate_keys.each do |key|
+          Decidim::Debates::Debate.create!(
+            component: @debates_component,
+            title: I18n.available_locales.index_with { |locale| I18n.t("decidim.components.debates.initial_debates.titles.#{key}", locale: locale) },
+            description: I18n.available_locales.index_with { |_locale| "" },
+            author: @user_group,
+            start_time: nil,
+            end_time: nil
+          )
+        end
       end
 
       def add_members_as_private_users
